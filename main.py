b61d3b300d5ec9eacae1c7586628d574769b5915
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-03 18:40:11 -0500

    tweak coordinates and if statement

--- main.py
+++ main.py
@@ -36,14 +36,24 @@ answer_state = screen.textinput(title=f"/50 Guess the State", prompt="What's ano
 
 
 if answer_state in state:
+    # If the user State exist in data['state']
+    state = data[data['state'] == answer_state]
+
+    # Provide coordinates if exist
+    coordinates = list(zip(state['x'], state['y']))
+
+    # Plot location
     writer.penup()
     writer.color('#000fff')
     writer.shapesize(.25, .25)
-    writer.goto(coordinates[0])
+    writer.goto(coordinates[0]) # location to go to
     writer.write(answer_state, align="left", font=("Arial", 8, "normal"))
 else:
-    print(False)
+    writer.penup()
+    writer.write(answer_state, align="left", font=("Arial", 8, "normal"))
 
 
 
-turtle.mainloop()
+
+
+# turtle.mainloop()
