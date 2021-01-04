a9342217c5a9266df6cc49fb02e4dfe00c35da25
Nesta Parchment <saint.clever.np@gmail.com>
2021-01-04 00:25:14 -0500

    create 'reset', 'off' and 'winner' if statement

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ data = pd.read_csv('50_states.csv')
 
 game_on = True
 score = 0
+correct_answers = []
 
 
 while game_on:
@@ -32,41 +33,55 @@ while game_on:
 
     # coordinates
     coordinates = list(zip(data['x'], data['y']))
-    
 
-    answer_state = screen.textinput(title=f"{score} / 50 Guess the State", prompt="What's another state's name?").title()
+
+    user_answer = screen.textinput(title=f"{score} / 50 Guess the State", prompt="What's another state's name?").title()
 
 
     # write to screen
-    writer = turtle.Turtle(shape='circle')
+    writer = turtle.Turtle()
     writer.penup()
-    writer.shapesize(.25, .25)
+    writer.shape('circle')
 
 
-    if answer_state in state:
+
+    if user_answer in state:
         # If the user provided State exist in data['state']
-        state = data[data['state'] == answer_state]
+        state = data[data['state'] == user_answer]
 
         # Provide coordinates if exist
         coordinates = list(zip(state['x'], state['y']))
 
         # Plot location
-        writer.color('#000fff')
+        writer.clear() # clear previous text
+        writer.color('#086a80')
+        writer.shapesize(stretch_wid=.20, stretch_len=.20)
         writer.goto(coordinates[0]) # location to go to
-        writer.write(answer_state, align='center', font=('Arial', 10, 'normal'))
-        score += 1
+        writer.write(user_answer, align='left', font=('Arial', 12, 'normal'))
 
-    elif answer_state == 'Off': # Game status
-        game_on = False
+        # Check to see if the users asnwer was or wan't in correct answers
+        # if not append and add 1 to score else do nothing
+        if user_answer not in correct_answers:
+            correct_answers.append(user_answer)
+            score += 1
+            
+    # elif user_answer == 'Reset':
+    #     screen.clear()
+    #     correct_answers = []
+    #     score = 0
 
+    # elif user_answer == 'Off': # Game status
+    #     game_on = False
+
+    # elif len(correct_answers) == len(state):
+    #     writer.clear() # clear previous text
+    #     writer.home() # return home
+    #     writer.write('You did it!', align='center', font=('Arial', 12, 'normal'))
+
+        
     else:
-        writer.clear()
-        writer.write('Try again please', align='center', font=('Arial', 10, 'normal'))
+        writer.clear() # clear previous text
+        writer.write('Try again please', align='center', font=('Arial', 12, 'normal'))
 
     
-
-
-
-
-
 turtle.mainloop()
